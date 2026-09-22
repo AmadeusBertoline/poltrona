ac1c620dc884a278346aa378c317a2336fc41e8a
Amadeus Bertoline <amadeusbertoline123@gmail.com>
2026-09-22 14:31:28 -0300

    chore: iniciando a criação do Gerente de um cinema

--- src/main/java/poltrona/entity/Gerente.java
+++ src/main/java/poltrona/entity/Gerente.java
@@ -1,5 +1,19 @@
 package poltrona.entity;
 
+import java.time.LocalDate;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+import poltrona.enums.usuario.StatusConta;
+
+@Entity
+@Table(name = "gerentes")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Gerente extends Usuario {
+
+    public Gerente(String nome, String email, String senha, String cpf, LocalDate dataNascimento, StatusConta status) {
+        super(nome, email, senha, cpf, dataNascimento);
+    }
 
 }
